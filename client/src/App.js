ba1f2518225ab76da2b94ad1f50f3bfa9e2b071f
Heang Ly <heanglypa@gmail.com>
2020-08-28 13:01:30 -0400

    final

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Route, Switch} from 'react-router-dom';
+import './App.css';
+import Recipes from './components/Recipes';
+import Navbar from './components/Navbar';
+import Top5 from './components/Top5';
+// import Footer from './components/Footer';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Switch>
+        <Route exact path='/' component={Recipes}/>
+        <Route exact path='/recipes' component={Recipes}/>
+        <Route exact path='/recipes/top5' component={Top5}/>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Footer.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import '../styles/Footer.css'
+
+class Footer extends Component{
+  render(){
+    return(
+      <div className='Footer'>
+        <p className='Footer-built'> Built by team 5 Guys</p>
+        <p> Copyright &copy; 2020 All Rights Reserved</p>
+      </div>
+    )
+  }  
+
+}
+
+export default Footer;
